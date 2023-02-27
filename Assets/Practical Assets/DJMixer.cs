975539d7777e0cd64937f3f9f7539c3b412f3f46
Jonathan Hook <jonathan.hook@york.ac.uk>
2023-02-27 13:34:25 +0000

    Added Unity project

---
+++ Assets/Practical Assets/DJMixer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DJMixer : MonoBehaviour 
+{
+	public AudioClip[] tracks;
+    public float offVolume = 0.0f;
+    public float onVolume = 1.0f;
+
+	private AudioSource[] players;
+
+	void Start () 
+	{
+		InitSources ();
+	}
+
+	public void SetTrackState(int trackId, bool playing)
+	{
+		if (trackId >= 0 && trackId < players.Length) 
+		{
+			players [trackId].volume = playing ? onVolume : offVolume;
+		} 
+		else 
+		{
+			print ("Invalid track ID: " + trackId);
+		}
+	}
+
+	private void InitSources()
+	{
+		players = new AudioSource[tracks.Length];
+		for (int i = 0; i < tracks.Length; i++)
+		{
+			AudioSource s = this.gameObject.AddComponent<AudioSource>();
+			s.clip = tracks[i];
+			s.loop = true;
+			s.volume = offVolume;
+			s.Play();
+
+			players[i] = s;
+		}
+	}
+}
