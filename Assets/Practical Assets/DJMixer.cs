d44f58dfa7c5d8a3ec0412af566bea7c7b7746c9
Jonathan Hook <jonathan.hook@york.ac.uk>
2024-05-04 10:05:25 +0100

    Reformatted with CSharpier

--- Assets/Practical Assets/DJMixer.cs
+++ Assets/Practical Assets/DJMixer.cs
@@ -1,52 +1,52 @@
 ﻿using UnityEngine;
 
-public class DJMixer : MonoBehaviour 
+public class DJMixer : MonoBehaviour
 {
-	public AudioClip[] tracks;
+    public AudioClip[] tracks;
     public float offVolume;
     public float onVolume = 1.0f;
 
-	private AudioSource[] _players;
+    private AudioSource[] _players;
 
-	private void Start () 
-	{
-		InitSources ();
-	}
+    private void Start()
+    {
+        InitSources();
+    }
 
-	public void PlayTrack(int trackId)
-	{
-		SetTrackState (trackId, true);
-	}
-	
-	public void StopTrack(int trackId)
-	{
-		SetTrackState (trackId, false);
-	}
-	
-	private void SetTrackState(int trackId, bool playing)
-	{
-		if (trackId >= 0 && trackId < _players.Length) 
-		{
-			_players [trackId].volume = playing ? onVolume : offVolume;
-		} 
-		else 
-		{
-			print ("Invalid track ID: " + trackId);
-		}
-	}
+    public void PlayTrack(int trackId)
+    {
+        SetTrackState(trackId, true);
+    }
 
-	private void InitSources()
-	{
-		_players = new AudioSource[tracks.Length];
-		for (var i = 0; i < tracks.Length; i++)
-		{
-			var s = gameObject.AddComponent<AudioSource>();
-			s.clip = tracks[i];
-			s.loop = true;
-			s.volume = offVolume;
-			s.Play();
+    public void StopTrack(int trackId)
+    {
+        SetTrackState(trackId, false);
+    }
 
-			_players[i] = s;
-		}
-	}
+    private void SetTrackState(int trackId, bool playing)
+    {
+        if (trackId >= 0 && trackId < _players.Length)
+        {
+            _players[trackId].volume = playing ? onVolume : offVolume;
+        }
+        else
+        {
+            print("Invalid track ID: " + trackId);
+        }
+    }
+
+    private void InitSources()
+    {
+        _players = new AudioSource[tracks.Length];
+        for (var i = 0; i < tracks.Length; i++)
+        {
+            var s = gameObject.AddComponent<AudioSource>();
+            s.clip = tracks[i];
+            s.loop = true;
+            s.volume = offVolume;
+            s.Play();
+
+            _players[i] = s;
+        }
+    }
 }
